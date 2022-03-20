d86fd1a1cdc142a44e7b486cd6e0420e00158105
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-20 11:32:19 +0500

    Added Timer class and elements

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
-console.log('Linked Successfully')
+class Timer {
+  constructor(durationInput, startButton, pauseButton){
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListerner('click', this.start);
+  }
+
+  start(){
+    console.log('Start the timer')
+  }
+}
