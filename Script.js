84402d6d9bf88f3d78fa1f5ee5511fd18ae6d38f
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-20 12:50:57 +0500

    handling interval and target DOM

--- Script.js
+++ Script.js
@@ -3,11 +3,28 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-
-    this.startButton.addEventListerner('click', this.start);
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause)
   }
 
-  start(){
-    console.log('Start the timer')
+  start = () => {
+    this.tick()
+    this.intervalId = setInterval(this.tick, 1000)
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  }
+
+  tick = () => {
+    console.log('tick')
   }
 }
+
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton)
+
