b327f35c0a74bfbe76792d24bbaf70489af59133
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-23 13:54:58 +0500

    Added functions to deal with Animations
    My Timer class now takes an optional parameter, which is an object consisting of 3 different functions, which will be responsible for handling the timer animations.
    onStart function initiates the animations
    onTick function plays the animation every second
    onComplete function resets the animation to default

--- Script.js
+++ Script.js
@@ -1,13 +1,22 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton){
+  constructor(durationInput, startButton, pauseButton, callbacks){
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if(callbacks){
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
     this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause)
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
+    if (this.onStart){
+      this.onStart();
+    }
     this.tick();
     this.intervalId = setInterval(this.tick, 1000);
   };
@@ -19,10 +28,16 @@ class Timer {
   tick = () => {
     if (this.remainingTime <= 0){
       this.pause();
+      if(this.onComplete){
+        this.onComplete();
+      }
     }else {
       // On left, we are calling the setter function and equating the value as a parameter.
       // On right, we are calling the getter function and manipulating its value.
       this.remainingTime = this.remainingTime - 1;
+      if(this.onTick){
+        this.onTick();
+      }
     }
   }
 
@@ -40,5 +55,15 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(){
+    console.log('Timer started')
+  },
+  onTick(){
+    console.log('Time ticked')
+  },
+  onComplete(){
+    console.log('Timer Completed')
+  }
+})
 
