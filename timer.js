98657c9f0522abbc7f5898b87ae91e1d4cd0cc1b
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-23 14:03:59 +0500

    Extracted Timer class to separate file
    Cleaned up code by separating the class based logic to a separate file name timer.js

---
+++ timer.js
@@ -0,0 +1,51 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks){
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if(callbacks){
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart){
+      this.onStart();
+    }
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  }
+
+  tick = () => {
+    if (this.remainingTime <= 0){
+      this.pause();
+      if(this.onComplete){
+        this.onComplete();
+      }
+    }else {
+      // On left, we are calling the setter function and equating the value as a parameter.
+      // On right, we are calling the getter function and manipulating its value.
+      this.remainingTime = this.remainingTime - 1;
+      if(this.onTick){
+        this.onTick();
+      }
+    }
+  }
+
+  get remainingTime(){
+    return parseFloat(this.durationInput.value);
+  }
+
+  set remainingTime(time){
+    this.durationInput.value = time
+  }
+}
